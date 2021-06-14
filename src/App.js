452bee106d61c4f7aeadd24af4dad2d575fbcfa9
Alessandro Lo Presti <mitlopresti@hotmail.it>
2021-06-14 10:16:04 +0200

    theme and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  Container,
+  createMuiTheme,
+  ThemeProvider,
+  CssBaseline,
+} from "@material-ui/core";
+import { useMemo } from "react";
 
 function App() {
+  const theme = useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: "dark",
+        },
+      }),
+    []
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              Home
+            </Route>
+            <Route path="/:id">Dettaglio</Route>
+          </Switch>
+        </Router>
+      </Container>
+    </ThemeProvider>
   );
 }
 
