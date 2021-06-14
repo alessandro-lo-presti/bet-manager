0d2e9167f6b337327126d8710cb163a9dcff9bb1
Alessandro Lo Presti <mitlopresti@hotmail.it>
2021-06-14 11:31:11 +0200

    betlist page connected to store

--- src/components/bet-list/BetList.js
+++ src/components/bet-list/BetList.js
@@ -1,16 +1,36 @@
 import { useEffect } from "react";
+import { connect } from "react-redux";
+import {
+  betListErrorAction,
+  betListSelector,
+  betListSuccessAction,
+} from "../../redux/slices/betSlice";
 import { ApiServices } from "../../services/ApiServices";
 
+const mapStateToProps = (state) => ({ betList: betListSelector(state) });
+
+const mapDispatchToProps = (dispatch) => ({
+  betListSuccess: (betList) => dispatch(betListSuccessAction(betList)),
+  betListError: () => dispatch(betListErrorAction()),
+});
+
 function BetList(props) {
+  const { betList, betListSuccess, betListError } = props;
+
   useEffect(() => {
-    ApiServices.betListApi().then((data) => console.log(data));
-  }, []);
+    ApiServices.betListApi().then(betListSuccess).catch(betListError);
+  }, [betListSuccess, betListError]);
 
   return (
     <div>
-      <p>BetList</p>
+      {betList.map((bet) => (
+        <div key={bet.idEvento}>
+          <p>{bet.descrizione}</p>
+          <p>{bet.odds}</p>
+        </div>
+      ))}
     </div>
   );
 }
 
-export default BetList;
+export default connect(mapStateToProps, mapDispatchToProps)(BetList);
